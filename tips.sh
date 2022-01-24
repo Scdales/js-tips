57f9b9427dbbcc05605971d0584ec8f56b5aaf48
Sean Daley <sean.daley@imgarena.com>
2022-01-24 17:42:15 +0000

    recursively delete folders by title

---
+++ tips.sh
@@ -0,0 +1,5 @@
+# Print out a list of directories to be deleted:
+find . -name 'node_modules' -type d -prune
+
+#Delete directories from the current working directory:
+find . -name 'node_modules' -type d -prune -exec rm -rf '{}' +
