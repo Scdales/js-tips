cb672532ab6bcbb4c7e5ecabaf67c871e25b44a6
Scdales <28219661+Scdales@users.noreply.github.com>
2022-09-27 23:45:49 +0100

    path search

--- tips.sh
+++ tips.sh
@@ -3,3 +3,6 @@ find . -name 'node_modules' -type d -prune
 
 #Delete directories from the current working directory:
 find . -name 'node_modules' -type d -prune -exec rm -rf '{}' +
+
+# scans $PATH and greps for 'brew'
+for cmd in $(echo $PATH | tr ":" "\n"); do ls -al $cmd 2>/dev/null; done | grep -i brew
